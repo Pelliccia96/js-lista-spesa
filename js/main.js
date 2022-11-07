b293da1bb72f2e1831480dbcd49db314ae5055a8
Pelliccia96 <fra.pelliccia96@gmail.com>
2022-11-07 15:47:31 +0100

    gg ez

--- js/main.js
+++ js/main.js
@@ -1,8 +1,12 @@
 const list = ["Pane", "Farina", "Acqua", "Latte", "Biscotti", "Pasta"];
+console.log(list);
+const listEl = document.getElementById("list");
 
 let i = 0;
 
-while (i < list.length - 1) {
-    i++
+while (i < list.length) {
     console.log(list [i]);
+    i++
 }
+
+listEl.innerHTML += list;
