d15ac389760152ecc12ca490da6cb6dd2fbc42a0
Pelliccia96 <fra.pelliccia96@gmail.com>
2022-11-07 19:20:14 +0100

    Bug Fix

--- js/main.js
+++ js/main.js
@@ -2,16 +2,10 @@ const list = ["Pane", "Farina", "Acqua", "Latte", "Biscotti", "Pasta"];
 console.log(list);
 const listEl = document.getElementById("list");
 
-const listItems = document.querySelector("ul");
-const listItem = document.createElement("li");
-
 let i = 0;
 
 while (i < list.length) {
     console.log(list [i]);
-    listItem.innerHTML += list;
+    listEl.innerHTML = (list);
     i++
 }
-
-listEl.innerHTML += list;
-listItems.append(listItem);
